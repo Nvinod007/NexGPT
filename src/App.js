cd22d572669dde59fec12bef0047fc239ae57436
Nvinod007 <vinodkumar.nelanakula@gmail.com>
2024-05-06 09:35:45 +0530

    Added tailwind css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-3xl font-bold text-green-500">
       Let's Begin our project
     </div>
   );
