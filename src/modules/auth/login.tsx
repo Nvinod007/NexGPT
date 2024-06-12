3ab196027421f022bf0f3c0dd205a7d887d6b70e
Nvinod007 <vinodkumar.nelanakula@gmail.com>
2024-06-13 00:02:32 +0530

    Added validation for email and password

--- src/modules/auth/login.tsx
+++ src/modules/auth/login.tsx
@@ -1,13 +1,41 @@
 
-import { useState } from "react";
+import { useState, useRef} from "react";
 import Header from "../header";
 
-const Login = () => {
-    const [isSignInForm, setIsSignInForm] = useState(true);
+import { checkValidData } from "../shared/utils/validate";
 
-    const toggleSignInForm = () => {
-      setIsSignInForm(!isSignInForm);
-    };
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage] = useState<string|null>(null);
+
+
+    const name = useRef(null);
+    const email = useRef<HTMLInputElement>(null);
+    const password = useRef<HTMLInputElement>(null);
+
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  const handleButtonClick = () => {
+    console.log(
+      "email?.current?.value",
+      email,
+      email?.current?.value,
+    );
+    if (!email?.current?.value?.trim() || !password?.current?.value?.trim()) {
+      console.log("in if");
+      setErrorMessage("Enter email and password");
+      return;
+    }
+    const message = checkValidData(
+      email?.current?.value || "",
+      password.current?.value || ""
+    );
+    setErrorMessage(message);
+    if (message) return;
+
+  };
   return (
     <div>
       <Header />
@@ -17,29 +45,39 @@ const Login = () => {
           src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
           alt="logo"
         />
-        <form className="flex flex-col w-3/12 top-0 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="flex flex-col w-full md:w-4/6 lg:w-5/12 top-0 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+        >
           <h1 className="font-bold text-3xl py-4">
             {isSignInForm ? "Sign In" : "Sign Up"}
           </h1>
 
           {!isSignInForm && (
             <input
+              ref={name}
               type="text"
               placeholder="Full Name"
               className="p-4 my-4 w-full bg-gray-700"
             />
           )}
           <input
+            ref={email}
             type="text"
             placeholder="Email Address"
             className="p-4 my-4 w-full bg-gray-700"
           />
           <input
+            ref={password}
             type="password"
             placeholder="Password"
             className="p-4 my-4 w-full bg-gray-700"
           />
-          <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+          <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+          <button
+            className="p-4 my-6 bg-red-700 w-full rounded-lg"
+            onClick={handleButtonClick}
+          >
             {isSignInForm ? "Sign In" : "Sign Up"}
           </button>
           <div className="flex flex-row py-4 items-end ">
